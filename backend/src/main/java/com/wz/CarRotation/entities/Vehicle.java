0dfa106e3e92773422c9e236fc011814bcd8719d
Welington Zanon <welington_zanon@hotmail.com>
2021-05-21 09:41:53 -0300

    Entity/Repository user, vehicle

---
+++ backend/src/main/java/com/wz/CarRotation/entities/Vehicle.java
@@ -0,0 +1,81 @@
+package com.wz.CarRotation.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "tb_vehicle")
+public class Vehicle implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String brand;
+	private String model;
+	private Integer year;
+	
+	public Vehicle() {		
+	}
+
+	public Vehicle(String brand, String model, Integer year) {
+		this.brand = brand;
+		this.model = model;
+		this.year = year;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vehicle other = (Vehicle) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}	
+	
+}
