6004f773959bfd113de897773e250a6370c176dd
Welington Zanon <welington_zanon@hotmail.com>
2021-05-21 08:23:46 -0300

    project created

---
+++ backend/src/main/java/com/wz/CarRotation/CarRotationApplication.java
@@ -0,0 +1,13 @@
+package com.wz.CarRotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRotationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wz/CarRotation/CarRotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wz.CarRotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
